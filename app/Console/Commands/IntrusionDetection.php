9a5c1f43e7a07a1c5018fd610728d89a9205c10e
Rob Ware <r.ware@ulster.ac.uk>
2026-04-20 20:02:47 +0100

    added MAC checking

--- app/Console/Commands/IntrusionDetection.php
+++ app/Console/Commands/IntrusionDetection.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\AuthorisedVisitors;
+use App\Models\CurrentVisitors;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
@@ -15,14 +17,72 @@ class IntrusionDetection extends Command
      */
     public function handle()
     {
-        //
-        $user_search = "last | grep 'still logged in' |  awk '{print $1, $3, $5, $6, $7}'";
-        $fp = popen ($user_search, "r");
-        $users = array();
+        //Initialise the current_visitors table
+        $current_visitors_model = new CurrentVisitors;
+        $deleted_visitors = $current_visitors_model->delete();
+        $current_visitors_search = "last | grep 'still logged in' |  grep -v 'ansible' | grep '.' | awk '{print $1, $3, $5, $6, $7}'";
+        $fp = popen ($current_visitors_search, "r");
+        $current_visitors = array();
         while ($rec = fgets($fp))
         {
-            $users[] = trim($rec);
+            $current_visitors[] = trim($rec);
         }
-        print_r($users);
+        //print_r($current_visitors);
+        if(count($current_visitors) > 0)
+        {
+            foreach($current_visitors as $current_visitor)
+            {
+                $visitor_details = explode(' ',$current_visitor);
+                $name = $visitor_details[0];
+                $ip_address = $visitor_details[1];
+                $month = $visitor_details[2];
+                $year = date('Y');
+                $timestamp =  strtotime('JAN'.$year);
+                $month = date('m',$timestamp);
+                $day = $visitor_details[3];
+                $login = $visitor_details[4];
+                $login_time = "$year-$month-$day $login:00";
+                $visitor_mac_search = "ip neigh | grep '$ip_address'";
+                $fp = popen ($visitor_mac_search, "r");
+                $visitor_macs = array();
+                while ($rec = fgets($fp))
+                {
+                    $visitor_macs[] = trim($rec);
+                }
+                if(count($visitor_macs) > 1)
+                {
+                    //log and email a warning
+                }
+                elseif(count($visitor_macs) < 1)
+                {
+                    //log and email a warning
+                }
+                else
+                {
+                    $mac_address = $visitor_macs[0];
+                    $authorised_visitor = AuthorisedVisitors::where('mac_address', $mac_address)
+                                                        ->where('name', $name)
+                                                        ->where('ip_address', $ip_address)
+                                                        ->first();
+                    if(!$authorised_visitor)
+                    {
+                        //email a warning
+                        $authorised = 'no';
+                    }
+                    else
+                    {
+                        $authorised = 'yes';
+                    }
+                }
+                $new_visitor = new CurrentVisitors;
+                $new_visitor->name = $name;
+                $new_visitor->ip_address = $ip_address;
+                $new_visitor->mac_address = $mac_address;
+                $new_visitor->authorised = $authorised;
+                $new_visitor->login_time = $login_time;
+                print_r($new_visitor);die;
+
+            }
+        }
     }
 }
