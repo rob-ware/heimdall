a97af97b713073ecbf2a9901382d2d0fe6ae2442
Rob Ware <r.ware@ulster.ac.uk>
2026-04-20 15:29:21 +0100

    Altered AWK call for Debian

--- app/Console/Commands/IntrusionDetection.php
+++ app/Console/Commands/IntrusionDetection.php
@@ -16,7 +16,7 @@ class IntrusionDetection extends Command
     public function handle()
     {
         //
-        $user_search = "last | grep 'still logged in' |  awk '{print $1, $2, $4, $5, $6}'";
+        $user_search = "last | grep 'still logged in' |  awk '{print $1, $3, $4, $5, $6}'";
         $fp = popen ($user_search, "r");
         $users = array();
         while ($rec = fgets($fp))
