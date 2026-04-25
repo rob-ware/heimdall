9b7319ef55d500e13a12c3c74358d878118221d3
Rob Ware <r.ware@ulster.ac.uk>
2026-04-25 16:48:36 +0100

    Added server analysis dashboard

---
+++ app/Http/Controllers/ServerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CurrentVisitors;
+use App\Models\EnvAction;
+use App\Models\FailedLogin;
+use App\Models\ServerWatch;
+use App\Models\SudoEvent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class ServerController extends Controller
+{
+    //Provides analysis of server security
+
+    /**
+     * Show the current status of the server.
+     */
+    public function show()
+    {
+        //Current users on the server
+        $visitors = CurrentVisitors::orderBy('login_time', 'desc')->get();
+        if($visitors)
+        {
+            foreach($visitors as $visitor)
+            {
+                $visitor->login_time = date('D H:i', strtotime($visitor->login_time));
+                if($visitor->name == 'rob')
+                {
+                    $visitor->image = 'images/rob.jpg';
+                }
+                elseif($visitor->name == 'casey')
+                {
+                    $visitor->image = 'images/rob.jpg';
+                }
+                else
+                {
+                    $visitor->image = 'images/intruder.jpg.avif';
+                    $visitor->authorised  = 'no';
+                    $visitor->name  = 'intruder';
+                }
+                if($visitor->authorised  == 'no')
+                {
+                    $visitor->image = 'images/intruder.jpg.avif';
+                    $visitor->name  = 'intruder';
+                }
+            }
+        }
+        //.env analysis
+        $env_actions = EnvAction::orderBy('timestamp', 'desc')->get();
+        if($env_actions)
+        {
+            foreach($env_actions as $env_action)
+            {
+                $env_action->timestamp = date('D H:i', strtotime($env_action->timestamp));
+            }
+        }
+        //Failed logins
+        $failed_logins = FailedLogin::orderBy('login_time', 'desc')->get();
+        if($env_actions)
+        {
+            foreach($failed_logins as $failed_login)
+            {
+                $failed_login->login_time = date('D H:i', strtotime( $failed_login->login_time));
+            }
+        }
+        //Root Access
+        $sudo_events = SudoEvent::orderBy('timestamp', 'desc')->get();
+        if($sudo_events)
+        {
+            foreach($sudo_events as $sudo_event)
+            {
+                $sudo_event->timestamp = date('D H:i', strtotime($sudo_event->timestamp));
+            }
+        }
+        //CPU and RAM usage
+        $server_watches = ServerWatch::orderBy('created_at')->get();
+        $cpu_text = '0,0,0,0,0,0';
+        $ram_text = '0,0,0,0,0,0';
+        if($server_watches)
+        {
+            $cpu_text = '';
+            $ram_text = '';
+            foreach($server_watches as $server_watch)
+            {
+                $cpu_text = $cpu_text."$server_watch->cpu_percentage,";
+                $ram_text = $ram_text."$server_watch->ram_percentage,";
+            }
+            //remove trailing commas
+            $cpu_text = substr($cpu_text, 0, -1);
+            $ram_text = substr($ram_text, 0, -1);
+        }
+
+        return view('server.visitors')
+            ->with('visitors', $visitors)
+            ->with('env_actions', $env_actions)
+            ->with('failed_logins', $failed_logins)
+            ->with('sudo_events', $sudo_events)
+            ->with('cpu_text', $cpu_text)
+            ->with('ram_text', $ram_text);
+    }
+
+}
---
+++ resources/views/server/visitors.blade.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="20">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+
+<body>
+    <div style="padding-bottom: 40px;" class="container">
+        <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                    <a href="/"><i class="fa fa-code" style="font-size:24px;color:blue">></i></i></a>
+                </div>
+                <div style="text-align: right" class="col-sm-6 col-md-6 col-lg-16 col-xs-6">
+                    <a href="{{ env('APP_URL') }}"><i class="fa fa-sign-out" style="font-size:24px;color:red"></i></a>
+                </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <div id="container1">
+                    <script>
+                        Highcharts.chart('container1', {
+                            chart: {
+                                type: 'column'
+                            },
+                            title: {
+                                text: 'CPU Usage'
+                            },
+                            subtitle: {
+                                text:
+                                    '3 minute timeseries at 30 second intervals'
+                            },
+                            xAxis: {
+                                categories: ['Snapshot #1', 'Snapshot #2', 'Snapshot #3', 'Snapshot #4', 'Snapshot #5', 'Snapshot #6'],
+                                crosshair: true,
+                                accessibility: {
+                                    description: 'CPU Snapshots'
+                                }
+                            },
+                            yAxis: {
+                                min: 0,
+                                title: {
+                                    text: 'Percentage of CPU used'
+                                }
+                            },
+                            tooltip: {
+                                valueSuffix: ' %CPU'
+                            },
+                            plotOptions: {
+                                column: {
+                                    pointPadding: 0.2,
+                                    borderWidth: 0
+                                }
+                            },
+                            series: [
+                                {
+                                    name: '% CPU Use',
+                                    data: [{!! $cpu_text !!}]
+                                }
+                            ]
+                        });
+
+                    </script>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <div id="container2">
+                    <script>
+                        Highcharts.chart('container2', {
+                            chart: {
+                                type: 'column'
+                            },
+                            title: {
+                                text: 'RAM Usage'
+                            },
+                            subtitle: {
+                                text:
+                                    '3 minute timeseries at 30 second intervals'
+                            },
+                            xAxis: {
+                                categories: ['Snapshot #1', 'Snapshot #2', 'Snapshot #3', 'Snapshot #4', 'Snapshot #5', 'Snapshot #6'],
+                                crosshair: true,
+                                accessibility: {
+                                    description: 'RAM Snapshots'
+                                }
+                            },
+                            yAxis: {
+                                min: 0,
+                                title: {
+                                    text: 'Percentage of RAM used'
+                                }
+                            },
+                            tooltip: {
+                                valueSuffix: ' %RAM'
+                            },
+                            plotOptions: {
+                                column: {
+                                    pointPadding: 0.2,
+                                    borderWidth: 0,
+                                    color: '#FF0000'
+                                }
+                            },
+                            series: [
+                                {
+                                    name: 'Max RAM = 16GB',
+                                    data: [{!! $ram_text !!}]
+                                }
+                            ]
+                        });
+
+                    </script>
+                </div>
+            </div>
+        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <h8><strong>Server Logins</strong></h8><br><br>
+                @if($visitors)
+                    @foreach($visitors as $visitor)
+                        <div class="row">
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                <img src="{{ $visitor->image }}" alt="{{ $visitor->name }}" width="50"">
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                @if($visitor->authorised == 'no')
+                                    <span style="color: red;" >
+                                        {{ $visitor->login_time }}
+                                    </span>
+                                @else
+                                    {{ $visitor->login_time }}
+                                @endif
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                @if($visitor->authorised == 'no')
+                                    <span style="color: red;" >
+                                        {{ $visitor->name }}
+                                    </span>
+                                @else
+                                    {{ $visitor->name }}
+                                @endif
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                @if($visitor->authorised == 'no')
+                                    <span style="color: red;" >
+                                        {{ $visitor->ip_address }}
+                                    </span>
+                                @else
+                                    {{ $visitor->ip_address }}
+                                @endif
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                @if($visitor->authorised == 'no')
+                                    <span style="color: red;" >
+                                        {{ $visitor->mac_address }}
+                                    </span>
+                                @else
+                                    {{ $visitor->mac_address }}
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            There are no current users on the server
+                        </div>
+                    </div>
+                @endif
+            </div>
+
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <h8><strong>Actions against Recruit .env</strong></h8>
+                @if($env_actions)
+                    @foreach($env_actions as $env_action)
+                        <div class="row">
+                            <div class="col-sm-4 col-md-4 col-lg-4 col-xs-4">
+                                {{ $env_action->timestamp }}
+                            </div>
+                            <div class="col-sm-4 col-md-4 col-lg-4 col-xs-4">
+                                {{ $env_action->action}}
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            There are no current action against the Recruit .env file
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <h8><strong>Failed Login Attemps</strong></h8><br><br>
+                @if($failed_logins)
+                    @foreach($failed_logins as $failed_login)
+                        <div class="row">
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                <span style="color: red;" >
+                                     {{ $failed_login->user }}
+                                </span>
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                {{ $failed_login->login_time }}
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                    {{ $failed_login->protocol }}
+                                </div>
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                {{ $failed_login->ip_address }}
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            There are no current failed logins on the server
+                        </div>
+                    </div>
+                @endif
+            </div>
+
+            <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+                <h8><strong>Root Access</strong></h8>
+                @if($sudo_events)
+                    @foreach($sudo_events as $sudo_event)
+                        <div class="row">
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                <span style="color: red;" >
+                                     {{ $sudo_event->user }}
+                                </span>
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2 col-xs-2">
+                                {{ $sudo_event->timestamp }}
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                            There has been no recent access to the root user account.
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+</html>
+
